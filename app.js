3ebfa10217ebf54305879b59e0216c269eff422f
Vasilis-Rousis <vasilisrousis1998@gmail.com>
2022-07-09 14:10:31 +0300

    first commit

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    console.log(req.url);
-    res.writeHead(200, { 'content-type': 'text/html' });
-    res.write('<h1>Home Page</h1>');
-    res.end();
+    const url = req.url;
+    if(url === '/') {
+        console.log(req.url);
+        res.writeHead(200, { 'content-type': 'text/html' });
+        res.write('<h1>Home Page</h1>');
+        res.end();
+    }
+    else if(url === '/about') {
+        console.log(req.url);
+        res.writeHead(200, { 'content-type': 'text/html' });
+        res.write('<h1>About Page</h1>');
+        res.end();
+    }
+    else{
+        res.writeHead(404, { 'content-type': 'text/html' });
+        res.write('<h1>404 Page</h1>');
+        res.end();
+    }
 });
 
 server.listen(3000);
