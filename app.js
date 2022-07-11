f7af6722d3236e51c521baeca37a8c2ddf156330
Vasilis-Rousis <vasilisrousis1998@gmail.com>
2022-07-11 15:10:52 +0300

    second commit

--- app.js
+++ app.js
@@ -3,11 +3,13 @@ const path  = require('path');
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, './public')));
+app.use(express.static('./public'));
 
-app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, './navbar-app/index.html'));
-});
+// app.get('/', (req, res) => {
+//     res.sendFile(path.resolve(__dirname, './navbar-app/index.html'));
+//     adding to static assets
+//     SSR
+// });
 
 app.all('*', (req, res) => {
     res.status(404).send('Resource not found');
