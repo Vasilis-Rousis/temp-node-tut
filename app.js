2da0ef80c495e2f01cfe6e7b89501bdf11f3cf5a
Vasilis-Rousis <vasilisrousis1998@gmail.com>
2022-07-11 14:48:58 +0300

    second commit

--- app.js
+++ app.js
@@ -1,43 +1,18 @@
-const http = require('http');
-const { readFileSync } = require('fs');
+const express = require('express');
+const path  = require('path');
 
-const homePage = readFileSync('./navbar-app/index.html');
-const homeStyles = readFileSync('./navbar-app/styles.css');
-const homeImage = readFileSync('./navbar-app/logo.svg');
-const homeLogic = readFileSync('./navbar-app/browser-app.js');
+const app = express();
 
-const server = http.createServer((req, res) => {
-    const url = req.url;
-    if(url === '/') {
-        res.writeHead(200, { 'content-type': 'text/html' });
-        res.write(homePage);
-        res.end();
-    }
-    else if(url === '/about') {
-        res.writeHead(200, { 'content-type': 'text/html' });
-        res.write('<h1>About Page</h1>');
-        res.end();
-    }
-    else if(url === '/styles.css') {
-        res.writeHead(200, { 'content-type': 'text/css' });
-        res.write(homeStyles);
-        res.end();
-    }
-    else if(url === '/logo.svg') {
-        res.writeHead(200, { 'content-type': 'image/svg+xml' });
-        res.write(homeImage);
-        res.end();
-    }
-    else if(url === '/browser-app.js') {
-        res.writeHead(200, { 'content-type': 'text/javascript' });
-        res.write(homeLogic);
-        res.end();
-    }
-    else{
-        res.writeHead(404, { 'content-type': 'text/html' });
-        res.write('<h1>404 Page</h1>');
-        res.end();
-    }
+app.use(express.static(path.join(__dirname, './public')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './navbar-app/index.html'));
 });
 
-server.listen(3000);
+app.all('*', (req, res) => {
+    res.status(404).send('Resource not found');
+});
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
