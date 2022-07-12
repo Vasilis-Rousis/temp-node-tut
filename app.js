022c4ed2bf6e0256bb0c9304df3e12ec39e52521
Vasilis-Rousis <vasilisrousis1998@gmail.com>
2022-07-12 18:38:20 +0300

    second commit

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 const express = require('express');
-const path  = require('path');
-
 const app = express();
+const { products } = require('./data');
 
-app.use(express.static('./public'));
+app.get('/', (req, res) => {
+  res.send('<h1>Home Page</h1><a href="/api/products">Products</a>');
+});
 
-// app.get('/', (req, res) => {
-//     res.sendFile(path.resolve(__dirname, './navbar-app/index.html'));
-//     adding to static assets
-//     SSR
-// });
+app.get('/api/products', (req, res) => {
+  const newProducts = products.map(product => {
+    const { id, name, image } = product;
+    return { id, name, image };
+  }); 
 
-app.all('*', (req, res) => {
-    res.status(404).send('Resource not found');
+  res.json(newProducts);
 });
 
 app.listen(3000, () => {
