d44b28a47bde37675386d3404f7123a2a5a97ac7
Vasilis-Rousis <vasilisrousis1998@gmail.com>
2022-07-12 21:42:49 +0300

    third commit

--- app.js
+++ app.js
@@ -6,13 +6,18 @@ app.get('/', (req, res) => {
   res.send('<h1>Home Page</h1><a href="/api/products">Products</a>');
 });
 
-app.get('/api/products', (req, res) => {
-  const newProducts = products.map(product => {
-    const { id, name, image } = product;
-    return { id, name, image };
-  }); 
+app.get('/api/products/:productID', (req, res) => {
 
-  res.json(newProducts);
+  const {productID} = req.params;
+  const singleProduct = products.find(
+    product => product.id === Number(productID)
+  );
+
+  if (!singleProduct) {
+    res.status(404).send('Product not found');
+  }
+
+  res.json(singleProduct);
 });
 
 app.listen(3000, () => {
